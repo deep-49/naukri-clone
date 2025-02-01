2ceddb828fbac244bc003f8f2f8d663b7fb7a92d
deep-49 <132682141+deep-49@users.noreply.github.com>
2025-02-01 19:31:48 +0530

    initial setup done

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+export default function App(){
+  return (
+    <>
+     <div className="bg-slate-500 rounded-full p-5">
+      <h1 className="text-xl">Let's get started</h1>
+     </div>
+   
+    </>
+   
+  )
+}
