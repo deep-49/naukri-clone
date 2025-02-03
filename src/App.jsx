1266d11443a625f52b2c5843945003c47d0350e5
Trupti Chandwani <210303105014@paruluniversity.ac.in>
2025-02-03 16:39:00 +0530

    Build header

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 
 import Opportunities from "./pages/opportunities";
 import Participate from "./pages/participate";
+import Navbar from "./components/Navbar";
 
 
 export default function App(){
   return (
     <>
-     
+      <Navbar/>
       <Participate/>
       {/* <Opportunities/> */}
     
